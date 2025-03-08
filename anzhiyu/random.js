d4027d86b8ad64e32984205389efb9b8e25c09a7
xiaoliula <107171257+xiaoliula@users.noreply.github.com>
2025-03-08 15:28:18 +0000

    2025年3月8日23:26:49 更新文章数据 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2025/03/03/主题标签/"];function toRandomPost(){
+var posts=["2024/05/14/install报错问题/","2025/03/03/主题标签/","2022/10/23/关于代码注释/","2024/01/03/单线复用之使用机顶盒直连路由器且不占用带宽/","2022/10/29/闲置的手机秒变随身服务器，隔壁小孩都馋哭了/","2022/12/13/高数-不定积分/","2022/11/29/高数-公式/","2022/10/26/高数-导数/","2022/11/28/高数-导数的应用/","2022/11/09/高数-微分/","2023/03/01/高数-旋转体求体积/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[{"name":"Hexo","link":"https://hexo.io/zh-tw/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"}];
     var refreshNum = 1;
